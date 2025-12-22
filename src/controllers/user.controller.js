59244366d61d18a58d884daedf054d66cf0b90ed
Sandeepmothe <mothesandeep92@gmail.com>
2025-12-22 18:46:15 +0530

    add getCurrentUser and getWatchHistory routes; update user routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -248,6 +248,12 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, "Password changed successfully"));
 });
 
+const getCurrentUser = asyncHandler(async (req, res) => {
+  return res
+    .status(200)
+    .json(new ApiResponse(200, req.user, "Current user fetched successfully"));
+});
+
 const updateAccountDetails = asyncHandler(async (req, res) => {
   const { fullname, email } = req.body;
   const updates = {};
@@ -464,6 +470,7 @@ export {
   logoutUser,
   refreshAccessToken,
   changeCurrentPassword,
+  getCurrentUser,
   updateAccountDetails,
   updateUserAvatar,
   updateCoverImage,
